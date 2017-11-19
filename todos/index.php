c71bb801101886423912db9210cde765139b4da1
Jeff Puckett <jeff@jeffpuckett.com>
2017-11-19 02:56:32 +0000

    refactor create Controller class

--- todos/index.php
+++ todos/index.php
@@ -8,54 +8,85 @@ $db = require __DIR__.'/../pdo.php';
 require __DIR__.'/Repository.php';
 $repository = new Repository($db);
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $todo = parseRequestJson();
-    $todo['id'] = $repository->create($todo);
-    sendJson($todo);
-}
+echo (new Controller($repository))->sendResponse();
 
-if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-    validateId();
-    $repository->delete($_GET['id']);
-    successNoContent();
-}
-
-sendJson($repository->get());
-
-function parseRequestJson() : array
+class Controller
 {
-    return json_decode(file_get_contents('php://input'), true);
-}
+    protected $repository;
 
-function sendJson($data)
-{
-    header('Content-Type: application/json');
-    die(json_encode($data));
-}
-
-function badRequest()
-{
-    http_response_code(400);
-    die();
-}
-
-function successNoContent()
-{
-    http_response_code(204);
-    die();
-}
-
-function validateId()
-{
-    if (empty($_GET['id'])) {
-        badRequest();
+    public function __construct(Repository $repository)
+    {
+        $this->repository = $repository;
     }
 
-    $valid = filter_var($_GET['id'], FILTER_VALIDATE_INT, [
-        'options' => ['min_range' => 1]
-    ]);
+    public function sendResponse()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        return $this->$method();
+    }
 
-    if ($valid === false) {
-        badRequest();
+    protected function get() : string
+    {
+        return $this->sendJson($this->repository->get());
+    }
+
+    protected function post() : string
+    {
+        $todo = $this->parseRequestJson();
+        $todo['id'] = $this->repository->create($todo);
+        return $this->sendJson($todo);
+    }
+
+    protected function put() : string
+    {
+        $todo = $this->parseRequestJson();
+        if ($this->repository->update($todo)) {
+            return $this->sendJson($todo);
+        }
+    }
+
+    protected function delete()
+    {
+        $id = $this->getId();
+
+        if ($id === false) {
+            return $this->sendBadRequest();
+        }
+
+        if ($this->repository->delete($id)) {
+            return $this->sendSuccessNoContent();
+        }
+    }
+
+    protected function getId()
+    {
+        if (empty($_GET['id'])) {
+            return false;
+        }
+
+        return filter_var($_GET['id'], FILTER_VALIDATE_INT, [
+            'options' => ['min_range' => 1]
+        ]);
+    }
+
+    protected function parseRequestJson() : array
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    protected function sendJson($data) : string
+    {
+        header('Content-Type: application/json');
+        return json_encode($data);
+    }
+
+    protected function sendBadRequest()
+    {
+        http_response_code(400);
+    }
+
+    protected function sendSuccessNoContent()
+    {
+        http_response_code(204);
     }
 }
