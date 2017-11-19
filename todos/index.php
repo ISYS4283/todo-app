30f32060f14027ce77648070490ce4573e6612f0
Jeff Puckett <jeff@jeffpuckett.com>
2017-11-19 03:34:59 +0000

    fix error reporting response code

--- todos/index.php
+++ todos/index.php
@@ -2,12 +2,15 @@
 
 namespace ISYS4283\ToDo;
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require __DIR__.'/Repository.php';
 require __DIR__.'/Controller.php';
 $db = require __DIR__.'/../pdo.php';
 
-echo (new Controller(new Repository($db)))->sendResponse();
+try {
+    echo (new Controller(new Repository($db)))->sendResponse();
+} catch (\Throwable $e) {
+    http_response_code(500);
+    echo $e->getMessage();
+}
