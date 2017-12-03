ac14b23d2c17b815d5a812f9855c5cdf8e9fd5a3
Jeff Puckett <jeff@jeffpuckett.com>
2017-12-03 17:51:09 +0000

    test Strong Password rule

---
+++ grader/tests/Unit/StrongPasswordTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Rules\StrongPassword;
+use ISYS4283\ToDo\Authenticator;
+
+class StrongPasswordTest extends TestCase
+{
+    /**
+     * @return bool|string If passes, then true, else error message.
+     */
+    protected function check(string $password = null)
+    {
+        $token = $this->fakeToken(['password' => $password]);
+
+        $rule = new StrongPassword;
+
+        if ($rule->passes('password', $token)) {
+            return true;
+        }
+
+        return $rule->message();
+    }
+
+    public function fakeToken(array $override = []) : string
+    {
+        if (isset($override['password'])) {
+            return Authenticator::fake($override)->getToken();
+        }
+
+        return Authenticator::fake()->getToken();
+    }
+
+    public function test_validates_factory()
+    {
+        $pass = $this->check();
+        $this->assertTrue($pass);
+    }
+
+    public function test_too_short()
+    {
+        $password = 'Too Short';
+        $expected = 'Your password is too short! Must be at least 13 characters.';
+        $this->assertSame($expected, $this->check($password));
+    }
+}
