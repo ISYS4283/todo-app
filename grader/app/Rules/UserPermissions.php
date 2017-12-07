c4943234cfea59f19987bfa48959cb82783570b8
Jeff Puckett <jeff@jeffpuckett.com>
2017-12-07 20:09:47 +0000

    test assert create

--- grader/app/Rules/UserPermissions.php
+++ grader/app/Rules/UserPermissions.php
@@ -7,11 +7,13 @@ use App\Client;
 use App\BadResponse;
 use Exception;
 use GuzzleHttp\Exception\RequestException;
+use App\ToDo;
 
 class UserPermissions implements Rule
 {
     protected $message;
     protected $client;
+    protected $id;
 
     /**
      * Determine if the validation rule passes.
@@ -25,6 +27,7 @@ class UserPermissions implements Rule
         $this->client = new Client(request()->get('ip-address'), $value);
 
         $assertions = [
+            'create',
             'read',
         ];
 
@@ -63,6 +66,27 @@ class UserPermissions implements Rule
         $this->message = $e;
 
         return false;
+    }
+
+    protected function assertCreate()
+    {
+        $expected = factory(ToDo::class)->make()->toArray();
+
+        $actual = $this->client->post($expected);
+
+        if (!is_array($actual)) {
+            throw new Failure('Cannot create data. Response is not an array.');
+        }
+
+        if (!empty(array_diff_assoc($expected, $actual))) {
+            throw new Failure('Cannot create data. Actual does not match expected.');
+        }
+
+        if (!array_key_exists('id', $actual)) {
+            throw new Failure('Cannot create data. Response does not contain ID.');
+        }
+
+        $this->id = $actual['id'];
     }
 
     protected function assertRead()
