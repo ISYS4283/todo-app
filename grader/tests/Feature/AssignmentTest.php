305c710210b3b6a7ae1a2dfc4458fba0b96a33eb
Jeff Puckett <jeff@jeffpuckett.com>
2017-12-07 19:53:00 +0000

    move client tests to separate test

--- grader/tests/Feature/AssignmentTest.php
+++ grader/tests/Feature/AssignmentTest.php
@@ -5,8 +5,6 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use ISYS4283\ToDo\Authenticator;
-use App\Client;
-use App\ToDo;
 
 class AssignmentTest extends TestCase
 {
@@ -20,17 +18,6 @@ class AssignmentTest extends TestCase
             'ip-address' => 'http://localhost:' . getenv('TEST_SERVER_PORT'),
             'user-token' => getenv('REGULAR_USER_TOKEN')
         ], $override);
-    }
-
-    protected function getClient(string $token) : Client
-    {
-        $host = 'http://localhost:' . getenv('TEST_SERVER_PORT');
-        return new Client($host, $token);
-    }
-
-    protected function getUser() : Client
-    {
-        return $this->getClient(getenv('REGULAR_USER_TOKEN'));
     }
 
     public function test_creates_assignment()
@@ -49,48 +36,5 @@ class AssignmentTest extends TestCase
             ]))
             ->assertSessionHasErrors(['user-token'])
         ;
-    }
-
-    public function test_user_can_connect_to_server()
-    {
-        $user = $this->getUser();
-        $this->assertTrue(is_array($user->get()));
-    }
-
-    public function test_user_can_create_todo()
-    {
-        $user = $this->getUser();
-
-        $expected = factory(ToDo::class)->make()->toArray();
-
-        $actual = $user->post($expected);
-
-        $this->assertArraySubset($expected, $actual);
-        $this->assertArrayHasKey('id', $actual);
-
-        return $actual;
-    }
-
-    /**
-     * @depends test_user_can_create_todo
-     */
-    public function test_user_can_update_todo(array $todo)
-    {
-        $expected = factory(ToDo::class)->make()->toArray();
-        $expected['id'] = $todo['id'];
-
-        $actual = $this->getUser()->put($expected);
-
-        $this->assertEquals($expected, $actual);
-
-        return $actual;
-    }
-
-    /**
-     * @depends test_user_can_update_todo
-     */
-    public function test_user_can_delete_todo(array $todo)
-    {
-        $this->assertTrue($this->getUser()->delete($todo['id']));
     }
 }
