6ce4568fb3e6f8697769bb12f72a51f5f0f9516e
Jeff Puckett <jeff@jeffpuckett.com>
2017-12-07 20:52:08 +0000

    add shibboleth

--- grader/resources/views/layout.blade.php
+++ grader/resources/views/layout.blade.php
@@ -1,1 +1,9 @@
 @extends('razorbacks::layout')
+
+@section('navbar-right')
+    @auth
+        <li><a href="/shibboleth-logout">Logout {{ Auth::user()->name }}</a></li>
+    @else
+        <li><a href="/shibboleth-login">Login</a></li>
+    @endauth
+@endsection
