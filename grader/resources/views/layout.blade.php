e7a202777c8321c7793a06f65186741eaa2c8bd9
Jeff Puckett <jeff@jeffpuckett.com>
2017-12-08 16:06:30 +0000

    add tutorial link in nav

--- grader/resources/views/layout.blade.php
+++ grader/resources/views/layout.blade.php
@@ -1,5 +1,12 @@
 @extends('razorbacks::layout')
 
+@section('navbar')
+    <li><a href="https://github.com/ISYS4283/todo-app"
+       target='_blank' rel="noopener noreferrer">
+       Tutorial
+    </a></li>
+@endsection
+
 @section('navbar-right')
     @auth
         <li><a href="/shibboleth-logout">Logout {{ Auth::user()->name }}</a></li>
